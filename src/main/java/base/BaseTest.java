ae04de0533d32cb71c2510887a065cc5d6484c33
amanparkashofficial <amanparkashofficial@gmail.com>
2026-01-20 17:06:46 +0500

    update

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,6 +1,8 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
@@ -12,9 +14,11 @@ public class BaseTest {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
+        WebElement inputsLink = driver.findElement (By.linkText("Inputs"));
+        inputsLink.click();
 
         System.out.println(driver.getTitle());
-        driver.quit();
+        //driver.quit();
     }
     public static void main(String args[]){
         BaseTest test = new BaseTest();
