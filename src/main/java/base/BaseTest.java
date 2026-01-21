917d8accc01901eafd1c6612d91bbeec4b7154bc
amanparkashofficial <amanparkashofficial@gmail.com>
2026-01-21 23:28:27 +0500

    update

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class BaseTest {
 
     private WebDriver driver;
@@ -13,9 +15,22 @@ public class BaseTest {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
-        driver.manage().window().maximize();
-        WebElement inputsLink = driver.findElement (By.linkText("Inputs"));
+        //driver.manage().window().maximize();
+
+        //List<WebElement> Links = driver.findElements(By.tagName("a"));
+        //System.out.println(Links.size());
+
+        //WebElement inputsLink = driver.findElement (By.linkText("Inputs"));
+        //inputsLink.click();
+
+        WebElement inputsLink = driver.findElement (By.linkText("Shifting Content"));
         inputsLink.click();
+
+        WebElement inputsLink1 = driver.findElement (By.linkText("Example 1: Menu Element"));
+        inputsLink1.click();
+
+        List<WebElement> Links = driver.findElements(By.tagName("li"));
+        System.out.println(Links.size());
 
         System.out.println(driver.getTitle());
         //driver.quit();
