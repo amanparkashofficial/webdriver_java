1a637e23559afeb936da11f8579be2526d4ac050
amanparkashofficial <amanparkashofficial@gmail.com>
2026-01-24 01:46:23 +0500

    update

--- src/main/java/pages/DropdownPage.java
+++ src/main/java/pages/DropdownPage.java
@@ -2,7 +2,11 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DropdownPage {
     private WebDriver driver;
@@ -13,7 +17,17 @@ public class DropdownPage {
     }
 
     public void selectFromDropdown(String option) {
-        Select dropdownElement = new Select(driver.findElement(dropdown));
+        findDropdownElement().selectByVisibleText(option);
+    }
+
+    public List<String> getSelectedOptions() {
+        List<WebElement> selectedElements =
+                findDropdownElement().getAllSelectedOptions();
+        return selectedElements.stream().map(e->e.getText()).collect(Collectors.toList());
+    }
+
+    private Select findDropdownElement(){
+        return new Select(driver.findElement(dropdown));
     }
 
 }
